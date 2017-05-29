3332278b27fdcfdb55d842e1bed123a9c1965864
David Vega Lichacz <blackmorth@hotmail.com>
2017-05-29 09:45:13 +0200

    Working Payment Type

--- eclipse/credit-services/src/app/onboarding/onboarding.component.ts
+++ eclipse/credit-services/src/app/onboarding/onboarding.component.ts
@@ -10,31 +10,24 @@ import { Component, OnInit, OnChanges } from '@angular/core';
   styleUrls: ['./onboarding.component.css']
 })
 export class OnboardingComponent implements OnInit {
-
   public activations;
-  gotoConfirm = false;
-  gotoIne = false;
-
   constructor(private orderService: OrderService, private onboardingService: OnboardingService) { }
 
   ngOnInit() {
-
     this.activations = this.onboardingService.getActivation();
 
     this.onboardingService.getActivations().subscribe((data) => {
       this.activations = data;
     });
 
-    this.onboardingService.typeSubject.subscribe((data: string) => {
-      if(data.valueOf().toString() === 'credito'){
+    this.onboardingService.getType().subscribe((data: any) => {
+      if(data.type === 'credito'){
         this.onboardingService.setOnConfirm();
-      } else if(data.valueOf().toString() === 'aplazado'){
+      } else if(data.type === 'aplazado'){
         this.onboardingService.setOnIne();
       }
       this.onboardingService.setOnPayment();
     })
-
-
 
   }
 
