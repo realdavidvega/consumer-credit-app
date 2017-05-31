161de02bef3965e03332f508c1f23ded85832a6a
David Vega Lichacz <blackmorth@hotmail.com>
2017-05-31 21:06:55 +0200

    TODOs added

--- eclipse/credit-services/src/app/app.component.ts
+++ eclipse/credit-services/src/app/app.component.ts
@@ -1,6 +1,5 @@
-
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 import * as firebase from 'firebase';
 
@@ -10,7 +9,6 @@ import * as firebase from 'firebase';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
   ngOnInit() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBhEElZurKcBuZHBAYTpWFB4YMyAnot5Xo',
@@ -21,9 +19,20 @@ export class AppComponent implements OnInit {
       messagingSenderId: '351510165342'
 
     });
-
   }
-
-
-
 }
+
+
+//TODO: Terminar vista OnConfirm (arreglar estilo y meter un Saber Más de creditos al consumo)
+
+//TODO: Hacer vistas Finish y Cancel
+
+//TODO: Diseñar e implementar token de salida de Cancel/Finish
+
+//TODO: Hacer que se guarden los datos de registro en Array/Service. Metemos unos predefinidos, pero si hay nuevos, sobreescribimos
+
+//TODO: Conectar todo con Firebase (login/registro, datosCliente, tokenSalida)
+
+//TODO OPCIONAL: Implementar token de entrada: Order (POST) -> Firebase; Firebase -> (GET) Onboarding
+
+//TODO OPCIONAL: Posibilidad meter varias tarjetas y elegir una
